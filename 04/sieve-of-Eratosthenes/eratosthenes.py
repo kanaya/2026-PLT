aba327c1f9306efce968178984ba5cd76a9afd24
Ichi Kanaya <kanaya@pineapple.cc>
2026-05-13 08:36:50 +0900

    Sieve of Erathosthenes in Python.

---
+++ 04/sieve-of-Eratosthenes/eratosthenes.py
@@ -0,0 +1,10 @@
+# エラトステネスの篩
+
+def sieve(n):
+    primes = []
+    for i in range(2, n + 1):
+        if all(i % p != 0 for p in primes):
+            primes.append(i)
+    return primes
+
+print(sieve(100))
