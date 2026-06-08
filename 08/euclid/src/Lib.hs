301f8299480c4e2586be03dc4823f9ed75d0fac6
kanaya <kanaya@pineapple.cc>
2026-06-08 11:33:28 +0900

    Euclid.

--- 08/euclid/src/Lib.hs
+++ 08/euclid/src/Lib.hs
@@ -2,5 +2,13 @@ module Lib
     ( someFunc
     ) where
 
+euclid :: Int -> Int -> Int
+euclid a 0 = a
+euclid a b = euclid b (a `mod` b)
+
+a = 123456
+b = 234567
+c = euclid a b
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = print c
